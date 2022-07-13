14b4eddf6039bef36c094a4c73f30efdf991d175
JoSihun <kepler452k@naver.com>
2022-07-13 21:31:07 +0900

    2022.07.13(수) / 21:30

---
+++ SpringProject/src/test/java/com/skuniv/cgvr/CgvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skuniv.cgvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
