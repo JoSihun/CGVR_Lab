6dc5bb86c706130f691068819bd6b050c98cff17
YeomDongBin <yeomdongbin@naver.com>
2022-08-02 02:16:51 +0900

    2022-08-02 2:16

--- SpringProject/src/main/java/com/skuniv/cgvr/CgvrApplication.java
+++ SpringProject/src/main/java/com/skuniv/cgvr/CgvrApplication.java
@@ -3,11 +3,36 @@ package com.skuniv.cgvr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PreDestroy;
+
 @SpringBootApplication
 public class CgvrApplication {
 
+	/* SSH 터널링
+	private static SSHConnection conn;
+	public CgvrApplication() {
+		conn = new SSHConnection().openSSH( res -> {
+			if(!res) {
+				System.out.println("포트포워딩 실패!");
+				System.exit(0);
+			}
+		});
+	}
+
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CgvrApplication.class, args);
 	}
+
+	/* SSH 연결 종료
+	@PreDestroy
+	public void end() {
+		try {
+			conn.closeSSH();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	 */
 
 }
---
+++ SpringProject/src/main/java/com/skuniv/cgvr/notice/LectNotController.java
@@ -0,0 +1,12 @@
+package com.skuniv.cgvr.notice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LectNotController {
+    @GetMapping("/notice/lect")
+    public String allNotice() {
+        return "lectnotice";
+    }
+}
