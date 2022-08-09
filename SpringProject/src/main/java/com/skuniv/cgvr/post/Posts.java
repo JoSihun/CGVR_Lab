165908c5ec249101bd2e540c5a68642755f0949d
JoSihun <kepler452k@naver.com>
2022-08-09 21:06:43 +0900

    2022.08.09(화) / 21:05

---
+++ SpringProject/src/main/java/com/skuniv/cgvr/post/Posts.java
@@ -0,0 +1,45 @@
+package com.skuniv.cgvr.post;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(length = 50)
+    private String author;
+
+    @Column(name="regdate", updatable = false)
+    private LocalDateTime regDate;
+
+    @Column(name="moddate")
+    private LocalDateTime modDate;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
