eedec32f13c05960b0cabe3ce0fa6581aafc75ce
JoSihun <kepler452k@naver.com>
2022-08-10 16:51:49 +0900

    2022.08.10(수) / 16:50

---
+++ SpringProject/src/main/java/com/skuniv/cgvr/controller/board/NoticeBoardController.java
@@ -0,0 +1,2 @@
+package com.skuniv.cgvr.controller.board;public class NoticeController {
+}
--- SpringProject/src/main/java/com/skuniv/cgvr/controller/notice/AllNoticeController.java
+++ SpringProject/src/main/java/com/skuniv/cgvr/controller/notice/AllNoticeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AllNotController {
+public class NoticeAllController {
     @GetMapping("/notice/all")
     public String allNotice() {
         return "Notice/allnotice";
