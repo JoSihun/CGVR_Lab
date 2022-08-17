603affaa4c3b2940ab52c7f53e3a978695e5c23c
JoSihun <kepler452k@naver.com>
2022-08-17 17:38:42 +0900

    2022.08.17(수) / 17:40

--- SpringProject/src/main/java/com/skuniv/cgvr/domain/Category.java
+++ SpringProject/src/main/java/com/skuniv/cgvr/domain/Category.java
@@ -6,12 +6,7 @@ import javax.persistence.*;
 
 @Getter
 @Entity
-<<<<<<< HEAD:SpringProject/src/main/java/com/skuniv/cgvr/domain/Category.java
 @NoArgsConstructor
-=======
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
->>>>>>> YeomDongBin:SpringProject/src/main/java/com/skuniv/cgvr/domain/Tag.java
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,11 +14,8 @@ public class Category {
     @Column
     private String content;
     @Builder
-<<<<<<< HEAD:SpringProject/src/main/java/com/skuniv/cgvr/domain/Category.java
-    public Category(Integer id, String content) {
-=======
+
     public Category(Long id, String content) {
->>>>>>> YeomDongBin:SpringProject/src/main/java/com/skuniv/cgvr/domain/Tag.java
         this.id = id;
         this.content = content;
     }
