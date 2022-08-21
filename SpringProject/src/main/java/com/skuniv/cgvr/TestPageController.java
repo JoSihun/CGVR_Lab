356085d0db7f8302fbdb41f72f7047ce6d329755
JoSihun <kepler452k@naver.com>
2022-08-21 19:45:24 +0900

    2022.08.21(일) / 19:45

--- SpringProject/src/main/java/com/skuniv/cgvr/TestPageController.java
+++ SpringProject/src/main/java/com/skuniv/cgvr/TestPageController.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestPageController {
 
     @GetMapping("/testpage")
-    public String testpage() { return "testpage_notice_frame"; }
+    public String testpage() { return "testpage_notice_frame_board"; }
 }
