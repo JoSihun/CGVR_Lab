694347696db4fd5e1653b45cb03da2f0cda1c7eb
YeomDongBin <yeomdongbin@naver.com>
2022-11-21 21:50:27 +0900

    "2022-11-21 / 21:50 / kakao api test"

--- SpringProject/src/main/java/com/skuniv/cgvr/controller/KakaoApiController.java
+++ SpringProject/src/main/java/com/skuniv/cgvr/controller/KakaoApiController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class KakaoApiController {
     // 이거 경로를 /***/*** 이런식으로 바꾸면 암만 해도 매핑이 안됩니다..
-    @GetMapping("/kakao")
+    @GetMapping("/test/kakao/api/address")
     public String kakaoApi() {
-        return "kakao_api.html";
+        return "kakao_api";
     }
 }
